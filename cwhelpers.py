7e503fe09e59efca0082ac94df6553030dcd171c
Imran Zamin Ali <ge20118@bristol.ac.uk>
2023-12-04 15:41:53 +0000

    did i break c?

--- cwhelpers.py
+++ cwhelpers.py
@@ -97,7 +97,7 @@ def run_stat_tests(profits):
 		result = list(zip(selected_rows['A'], selected_rows['B'], selected_rows['p-corr']))
 		print(result)
 
-def run_stats(n : int, df : pd.DataFrame, summary_entry):
+def run_stats(df : pd.DataFrame, summary_entry):
 	is_normal = True
 	for col in df.columns:
 		_, pvalue = stats.shapiro(df[col])
@@ -105,8 +105,7 @@ def run_stats(n : int, df : pd.DataFrame, summary_entry):
 			is_normal = False
 		summary_entry[f'{col} normality'] = pvalue
 
-	summary_entry['Reject null'] = not is_normal
-
+	summary_entry['All normal'] = is_normal
 	# check if comes from the same population
 	columns = [df[column] for column in df.columns]
 	is_two_dist = len(columns) == 2
@@ -122,7 +121,7 @@ def run_stats(n : int, df : pd.DataFrame, summary_entry):
 		further_test = pvalue < 0.05
 	elif (not is_normal) and is_two_dist:
 		test_name = 'Wilcoxon Signed Rank test'
-		stats.wilcoxon(*columns)
+		_, pvalue = stats.wilcoxon(*columns)
 	else:
 		test_name = 'Friedman Test'
 		_, pvalue = stats.friedmanchisquare(*columns)
@@ -130,7 +129,7 @@ def run_stats(n : int, df : pd.DataFrame, summary_entry):
 
 	summary_entry['Test Name'] = test_name
 	summary_entry['P Value'] = pvalue
-	summary_entry['Reject Null'] = pvalue < 0.05
+	summary_entry['Different populations'] = pvalue < 0.05
 
 	return summary_entry
 
@@ -158,6 +157,7 @@ def run_sessions(args):
 
 def create_threads(market_args, initial_seed, n):
 	profits = pd.DataFrame()
+	print(market_args)
 	with Pool(os.cpu_count()) as p:
 		p.map(run_sessions, [(market_args, initial_seed, instance) for instance in range(n)] )
 		p.close()
@@ -180,35 +180,59 @@ def part_a(n, market_args, initial_seed, r_vals):
 	stat_summary = pd.DataFrame()
 	fig, axes = plt.subplots(2,2)
 	for i in range(len(n)):
-		# profits = pd.DataFrame()
-		# with Pool(os.cpu_count()) as p:
-		# 	p.map(run_sessions, [(market_args, initial_seed, instance) for instance in range(n[i])] )
-		# 	p.close()
-		# 	p.join()
-		# for j in range(n[i]):
-		# 	filename = f'{j + initial_seed}_avg_balance.csv'
-		# 	file = open(filename, 'r')
-		# 	final_entry = (file.readlines()[-1]).split(',')
-		# 	file.close()
-		# 	profit_entry = {}
-		# 	for k in range(0, len(final_entry)-2, 2):
-		# 		col_name = final_entry[k].strip()
-		# 		col_val = final_entry[k+1]
-		# 		profit_entry[col_name] = float(col_val.strip())
-		# 	entry_df = pd.DataFrame([profit_entry])
-		# 	profits = pd.concat([profits, entry_df], ignore_index=True)
 		profits = create_threads(market_args, initial_seed, n[i])
 		summary_entry = {'n' : n[i], 'Trader ratio:' : ':'.join(str(r) for r in r_vals)}
-		summary_entry = run_stats(n, profits, summary_entry)
-		summmary_entry_df = pd.DataFrame([summary_entry])
-		stat_summary = pd.concat([stat_summary, summmary_entry_df], ignore_index=True)
+		summary_entry = run_stats(profits, summary_entry)
+		summmary_entry = pd.DataFrame([summary_entry])
+		stat_summary = pd.concat([stat_summary, summmary_entry], ignore_index=True)
 		sns.kdeplot(data=profits, fill=False, ax=axes[i, 0])
 		sns.boxplot(data=profits, ax=axes[i, 1])
 	return stat_summary
+
+def part_b(n, market_args, r_list, algos, initial_seed):
+	stat_summary = pd.DataFrame()
+	fig, axes = plt.subplots(1,len(r_list))
+	for i in range(len(r_list)):
+		r_vals = (r_list[i], 100 - r_list[i])
+		buyer_specs = get_traders_specs(algos, r_vals, 20)
+		seller_specs = get_traders_specs(algos, r_vals, 20)
+		market_args[2] = {'sellers': seller_specs, 'buyers': buyer_specs}
+		profits = create_threads(market_args, initial_seed, n)
+		summary_entry = {'Trader ratio' : ':'.join(str(r) for r in r_vals)}
+		summary_entry = run_stats(profits, summary_entry)
+		summary_entry = pd.DataFrame([summary_entry])
+		stat_summary = pd.concat([stat_summary, summary_entry], ignore_index=True)
+		sns.violinplot(data=profits, inner='box', cut = 0, ax=axes[i])
+	return stat_summary
 	
-	
+def part_c(n, market_args, r_vals, algos, initial_seed):
+	stat_summary = pd.DataFrame()
+	fig, axes =  None, None
+	if len(set(r_vals)) != 1:
+		fig, axes = plt.subplots(1, len(r_vals))
+	print(get_traders_specs(algos, r_vals, 20))
+	for i in range(len(r_vals)):
+		buyer_specs = get_traders_specs(algos, r_vals, 20)
+		seller_specs = get_traders_specs(algos, r_vals, 20)
+		market_args[2] = {'sellers': seller_specs, 'buyers': buyer_specs}
+		profits = create_threads(market_args, initial_seed, n)
+		summary_entry = {'Trader ratio' : ':'.join(str(r) for r in r_vals)}
+		print(profits.head())
+		print(profits.tail())
+		summary_entry = run_stats(profits, summary_entry)
+		summary_entry = pd.DataFrame([summary_entry])
+		stat_summary = pd.concat([stat_summary, summary_entry], ignore_index=True)
+		if len(set(r_vals)) == 1: # no need to permute if all elements are equal
+			sns.violinplot(data=profits, inner='box')
+			return stat_summary
+		else:
+			r_vals.append(r_vals.pop(0)) # rotates by 1
+			sns.violinplot(data=profits, inner='box', cut = 0, ax=axes[i])
+	return stat_summary
+			
+
 def part_d1(n, market_args, initial_seed):
-	
+	stat_summary = pd.DataFrame()
 	pps = pd.DataFrame()
 	with Pool(cpu_count()) as p:
 		p.map(run_sessions, [(market_args, initial_seed, i) for i in range(n)])
@@ -222,15 +246,21 @@ def part_d1(n, market_args, initial_seed):
 		file.close()
 		start_entry = np.average([float(value.split(',')[15].strip()) for value in lines[:5]])
 		end_entry = np.average([float(value.split(',')[15].strip()) for value in lines[-5:]])
-		pps_entry = pd.DataFrame({'start': [start_entry], 'end': [end_entry]})
+		pps_entry = pd.DataFrame({'inital pps': [start_entry], 'final pps': [end_entry]})
 		
 		pps = pd.concat([pps, pps_entry], ignore_index=True)
-		
-	run_stat_tests(pps)
+	
+	summary_entry = run_stats(pps_entry, {})
+	summary_entry = pd.DataFrame([summary_entry])
+	stat_summary = pd.concat([stat_summary, summary_entry], ignore_index=True)
+	fig, axes = plt.subplots(1,2)
+	sns.kdeplot(data=pps, fill=False, ax=axes[0])
+	sns.boxplot(data=pps, ax=axes[1])
+	return stat_summary
 
 
 # run n instances of market session. Collect the final profits for each trader for each session and store in a numpy array. Apply the appropriate hypothesis test
-def run_experiment(n, market_args, initial_seed):
+def run_experiment_old(n, market_args, initial_seed):
 	# create an empty dataframe
 	# traders = market_args[3]
 	# buyers = traders['buyers']
@@ -238,18 +268,48 @@ def run_experiment(n, market_args, initial_seed):
 	# algo_names = list({algo for (algo, _) in buyers+sellers})
 	# profits = pd.DataFrame(columns=algo_names)
 	profits = None
-	ave_profits = pd.DataFrame()
-	with Pool(os.cpu_count()) as p:
-		print('hello')
-		p.map(run_sessions, [(market_args, initial_seed, instance) for instance in range(n)] )
-		p.close()
-		p.join()
+	# ave_profits = pd.DataFrame()
+	# with Pool(os.cpu_count()) as p:
+	# 	print('hello')
+	# 	p.map(run_sessions, [(market_args, initial_seed, instance) for instance in range(n)] )
+	# 	p.close()
+	# 	p.join()
+	# for i in range(n):
+	# 	filename = f'{i + initial_seed}_avg_balance.csv'
+	# 	file = open(filename, 'r')
+	# 	final_entry = (file.readlines()[-1]).split(',')
+	# #print(len(final_entry))
+	# #print(final_entry)
+	# 	file.close()
+	# 	profit_entry = {}
+	# 	for j in range(0, len(final_entry)-2, 2):
+	# 		col_name = final_entry[j].strip()
+	# 		#print(col_name)
+	# 		col_val = final_entry[j+1]
+	# 		#print(col_val)
+	# 		profit_entry[col_name] = round(float(col_val.strip()))
+
+	# 	#print(profit_entry)
+	# 	entry_df = pd.DataFrame([profit_entry])
+	# 	#profits = profits.append(profit_entry, ignore_index=True)
+	   
+	# 	profits = pd.concat([profits, entry_df], ignore_index=True)
+	# #profits = ave_profits
+
+	# print(len(profits))
+	# run_stat_tests(profits)
+	# return profits
+	print('hello')
+	print(f'n: {n}')
 	for i in range(n):
-		filename = f'{i + initial_seed}_avg_balance.csv'
+		random.seed(initial_seed + i)
+		market_session(*market_args)
+		trial_id = market_args[0]
+		filename = f'{trial_id}_avg_balance.csv'
 		file = open(filename, 'r')
 		final_entry = (file.readlines()[-1]).split(',')
-	#print(len(final_entry))
-	#print(final_entry)
+		#print(len(final_entry))
+		#print(final_entry)
 		file.close()
 		profit_entry = {}
 		for j in range(0, len(final_entry)-2, 2):
@@ -262,33 +322,5 @@ def run_experiment(n, market_args, initial_seed):
 		#print(profit_entry)
 		entry_df = pd.DataFrame([profit_entry])
 		#profits = profits.append(profit_entry, ignore_index=True)
-	   
 		profits = pd.concat([profits, entry_df], ignore_index=True)
-	#profits = ave_profits
-
-	print(len(profits))
 	run_stat_tests(profits)
-	return profits
-	#print(f'n: {n}')
-#     for i in range(n):
-#         random.seed(initial_seed + i)
-#         market_session(*market_args)
-#         trial_id = market_args[0]
-#         filename = f'{trial_id}_avg_balance.csv'
-#         file = open(filename, 'r')
-#         final_entry = (file.readlines()[-1]).split(',')
-#         #print(len(final_entry))
-#         #print(final_entry)
-#         file.close()
-#         profit_entry = {}
-#         for j in range(0, len(final_entry)-2, 2):
-#             col_name = final_entry[j].strip()
-#             #print(col_name)
-#             col_val = final_entry[j+1]
-#             #print(col_val)
-#             profit_entry[col_name] = round(float(col_val.strip()))
-
-#         #print(profit_entry)
-#         entry_df = pd.DataFrame([profit_entry])
-#         #profits = profits.append(profit_entry, ignore_index=True)
-#         profits = pd.concat([profits, entry_df], ignore_index=True)
